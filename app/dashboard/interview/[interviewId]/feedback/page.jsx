7e3939d24784bcdfc4fa5ae4bdd04de8053690df
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2024-06-13 17:45:28 +0530

    feat: Add webcam functionality to interview feedback page
    This commit adds the necessary code changes to enable webcam functionality on the interview feedback page. It includes importing the `react-webcam` package, adding a state variable `webCamEnabled`, rendering the webcam component conditionally, and displaying the webcam feed along with interview feedback.

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+"use client"
 
-function Feedback() {
+import { db } from '@/utils/db'
+import { UserAnswer } from '@/utils/schema'
+import { eq } from 'drizzle-orm'
+import { ChevronsUpDown, PartyPopper } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+
+
+function Feedback({ params }) {
+  const router = useRouter()
+  const [feedbackList, setFeedbackList] = useState([])
+
+  useEffect(() => {
+    GetFeedback()
+  }, [])
+
+
+  const GetFeedback = async () => {
+    const result = await db.select()
+      .from(UserAnswer)
+      .where((eq(UserAnswer.mockIdRef, params.interviewId)))
+      .orderBy(UserAnswer.id)
+
+    console.log('result eeeeeeeeeeeeeeeeeee', result)
+    setFeedbackList(result)
+  }
+
   return (
-    <div>Feedback</div>
+    <div className='p-10 border rounded-lg my-10'>
+      <h2 className='flex gap-2 text-3xl font-bold text-green-500'><PartyPopper /> Congratulations!</h2>
+      {/* <h2 className='font-bold text-2xl'>Here is your interview feedback.</h2> */}
+      <h2 className='text-primary text-xl my-3'>Your overall interview rating: <strong>7/10</strong> </h2>
+
+      <h2 className='text-sm text-gray-500'>Find below interview question with correct answer, your answer and feedback for improvment</h2>
+
+      {feedbackList && feedbackList.map((item, index) => (
+        <Collapsible key={index}>
+          <CollapsibleTrigger className='flex justify-between text-left p-2 bg-secondary rounded-lg my-2 w-full'>
+            {item.question} <ChevronsUpDown className='h-5 w-5' />
+          </CollapsibleTrigger>
+          <CollapsibleContent className='px-5'>
+            <div className='flex flex-col gap-2'>
+              <h2 className='text-red-500 p-2 border rounded-lg'><strong>Rating:</strong>{item.rating}</h2>
+              <h2 className='p-2 border rounded-lg bg-red-50 text-sm text-red-900'><strong>Your Answer: </strong>{item.userAns} </h2>
+              <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer: </strong>{item.correctAns} </h2>
+              <h2 className='p-2 border rounded-lg bg-blue-50 text-sm text-blue-900'><strong>Feedback: </strong>{item.feedback} </h2>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+      ))
+      }
+
+      <Button onClick={()=>router.replace('/dashboard')}>Go Home</Button>
+    </div>
   )
 }
 
